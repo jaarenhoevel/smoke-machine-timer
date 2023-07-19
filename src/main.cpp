068cacf461b55389b85a793722efc8daef8b459c
János Arenhövel <jaarenhoevel@gmail.com>
2023-07-19 18:40:37 +0200

    Use controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <Arduino.h>
 
 #include <Machine.h>
+#include <Controller.h>
 
 #define TRIGGER_PIN   GPIO_NUM_25
 #define FEEDBACK_PIN  GPIO_NUM_32
 
 Machine *smokeMachine;
+Controller *controller;
 
 void setup() {
   smokeMachine = new Machine(TRIGGER_PIN, FEEDBACK_PIN);
+  controller = new Controller(smokeMachine);
 }
 
 void loop() {
   smokeMachine->process();
+  controller->process();
 }
