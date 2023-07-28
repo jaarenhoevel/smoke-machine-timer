9b46a01589e9ac66da6cd6713bd57de928b35669
János Arenhövel <jaarenhoevel@gmail.com>
2023-07-28 21:14:09 +0200

    Don't use fixed GPIO definitions

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,24 @@
 #include <Controller.h>
 #include <Interface.h>
 
-#define TRIGGER_PIN   GPIO_NUM_25
-#define FEEDBACK_PIN  GPIO_NUM_32
+#define TRIGGER_PIN       GPIO_NUM_25
+#define FEEDBACK_PIN      GPIO_NUM_32
+
+#define DISPLAY_CLK_PIN   GPIO_NUM_26
+#define DISPLAY_DATA_PIN  GPIO_NUM_27
 
 Machine *smokeMachine;
 Controller *controller;
 Interface *interface;
 
 void setup() {
-  smokeMachine = new Machine(TRIGGER_PIN, FEEDBACK_PIN);
-  controller = new Controller(smokeMachine);
-  interface = new Interface(controller, GPIO_NUM_27, GPIO_NUM_26);
+    smokeMachine = new Machine(TRIGGER_PIN, FEEDBACK_PIN);
+    controller = new Controller(smokeMachine);
+    interface = new Interface(controller, DISPLAY_DATA_PIN, DISPLAY_CLK_PIN);
 }
 
 void loop() {
-  smokeMachine->process();
-  controller->process();
-  interface->process();
+    smokeMachine->process();
+    controller->process();
+    interface->process();
 }
