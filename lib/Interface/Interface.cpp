6a9c1959be2507771808a4a8639dc5e8ff23529d
János Arenhövel <jaarenhoevel@gmail.com>
2023-07-28 21:10:07 +0200

    Move graphics commands to process method

--- lib/Interface/Interface.cpp
+++ lib/Interface/Interface.cpp
@@ -5,15 +5,20 @@ Interface::Interface(Controller *controller, gpio_num_t dataPin, gpio_num_t cloc
 
     this->u8g2 = new U8G2_SSD1306_128X64_NONAME_1_SW_I2C(U8G2_R0, dataPin, clockPin);
     this->u8g2->begin();
+}
+
+void Interface::process() {
+    if (millis() - this->lastUpdate < INTERFACE_UPDATE_INTERVAL) return;
+    this->lastUpdate = millis();
+    
+    std::string s = std::to_string(millis() / 1000);
 
     this->u8g2->firstPage();
     do {
         /* all graphics commands have to appear within the loop body. */    
         this->u8g2->setFont(u8g2_font_profont22_tr);
-        this->u8g2->drawStr(0,24,"Smoke Timer");
+        this->u8g2->drawStr(0, 24, "Smoke Timer");
+
+        this->u8g2->drawStr(0, 40, s.c_str());
     } while (this->u8g2->nextPage());
-}
-
-void Interface::process() {
-
 }
--- lib/Interface/Interface.h
+++ lib/Interface/Interface.h
@@ -3,6 +3,8 @@
 
 #include <Controller.h>
 
+#define INTERFACE_UPDATE_INTERVAL   1000
+
 class Interface {
     public:
         Interface(Controller *controller, gpio_num_t dataPin, gpio_num_t clockPin);
@@ -12,4 +14,6 @@ class Interface {
     private:
         Controller *controller;
         U8G2_SSD1306_128X64_NONAME_1_SW_I2C *u8g2;
+
+        uint32_t lastUpdate = 0;
 };
