17d28d401b1748e6652ab089c066296b7de31f64
János Arenhövel <jaarenhoevel@gmail.com>
2023-08-01 19:15:01 +0200

    Display basic overview

--- lib/Interface/Interface.cpp
+++ lib/Interface/Interface.cpp
@@ -1,24 +1,26 @@
 #include <Interface.h>
 
-Interface::Interface(Controller *controller, gpio_num_t dataPin, gpio_num_t clockPin) {
+Interface::Interface(Controller *controller, Machine *machine, gpio_num_t dataPin, gpio_num_t clockPin) {
     this->controller = controller;
+    this->machine = machine;
 
     this->u8g2 = new U8G2_SSD1306_128X64_NONAME_F_SW_I2C(U8G2_R0, dataPin, clockPin); 
     this->u8g2->begin();
 }
 
-void Interface::process() {
+void Interface::process() {    
     if (millis() - this->lastUpdate < INTERFACE_UPDATE_INTERVAL) return;
     this->lastUpdate = millis();
 
     this->u8g2->clearBuffer();
     
-    std::string s = std::to_string(millis() / INTERFACE_UPDATE_INTERVAL);
-
-    this->u8g2->setFont(u8g2_font_profont22_tr);
-    this->u8g2->drawStr(0, 24, "Smoke Timer");
-
-    this->u8g2->drawStr(0, 40, s.c_str());
+    this->drawOverview();
 
     this->u8g2->sendBuffer();
+}
+
+void Interface::drawOverview() {
+    this->u8g2->setFont(u8g2_font_profont22_tr);
+    this->u8g2->drawStr(0, 22, (this->machine->isReady()) ? "Ready." : "Heating...");
+    this->u8g2->drawStr(0, 44, (this->machine->isActive()) ? "Active." : "Idle.");
 }
--- lib/Interface/Interface.h
+++ lib/Interface/Interface.h
@@ -2,18 +2,22 @@
 #include <U8g2lib.h>
 
 #include <Controller.h>
+#include <Machine.h>
 
-#define INTERFACE_UPDATE_INTERVAL   750
+#define INTERFACE_UPDATE_INTERVAL   1000
 
 class Interface {
     public:
-        Interface(Controller *controller, gpio_num_t dataPin, gpio_num_t clockPin);
+        Interface(Controller *controller, Machine *macghine, gpio_num_t dataPin, gpio_num_t clockPin);
 
         void process();
     
     private:
         Controller *controller;
+        Machine *machine;
         U8G2_SSD1306_128X64_NONAME_F_SW_I2C *u8g2;
 
         uint32_t lastUpdate = 0;
+
+        void drawOverview();
 };
